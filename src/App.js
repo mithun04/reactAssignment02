4e166bb0ebf148eeb7429222753a68b6f4429b0f
Mithun <mithun.kumar@happiestminds.com>
2019-05-17 11:21:07 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0
+  }
+
+  addItem = () => {
+    if(document.getElementById("inputItem").value) {
+      const node = document.createElement("LI");
+      const textNode = document.createTextNode(document.getElementById("inputItem").value);
+      node.appendChild(textNode);
+      document.getElementById("myList").appendChild(node);
+      this.setState({count: this.state.count + 1});
+      document.getElementById("inputItem").value = "";
+    }
+  }
+
+  deleteItem = () => {
+    const list = document.getElementById("myList");
+    if (list.childNodes.length) {
+      list.removeChild(list.childNodes[this.state.count - 1]);
+      this.setState({count: this.state.count - 1});
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>TODO List</h1>
+        <label>Add TODOs to the list? </label>
+        <input id="inputItem" type="text"/>
+        <button onClick = {this.addItem}>Add {this.state.count}</button>
+        <button onClick = {this.deleteItem}>Delete</button>
+        <div>
+          <ul id="myList">
+
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
